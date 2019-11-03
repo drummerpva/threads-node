1cff5e6363406be20b3aa515100851d2c514f7e2
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-11-03 17:23:11 -0300

    Up

---
+++ threads.js
@@ -0,0 +1,16 @@
+const {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData
+} = require("worker_threads");
+
+console.log("Iniciou");
+
+if (isMainThread) {
+  const worker = new Worker(__filename, { workerData: 1 });
+  worker.on("message", message => console.log(message));
+} else {
+  const someMath = workerData + 2;
+  parentPort.postMessage(someMath);
+}
